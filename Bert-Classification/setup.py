2550264666d6c99c28242ca78e77189f18cecfa6
Si Chen <t-chsi@microsoft.com>
2019-07-19 13:03:35 +0800

    change

---
+++ Bert-Classification/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Semantic Analysis Model",
+    version="0.0.1",
+    description="Bert Semantic Analysis Model",
+    packages=["script"],
+    author="Si Chen",
+    license="MIT",
+    include_package_data=True,
+)
