77a651ae06f48df5c41bb9464d0349c8aa781e08
Si Chen <t-chsi@microsoft.com>
2019-07-23 17:59:56 +0800

    update python files

---
+++ QA/examples/transformer.py
@@ -0,0 +1,25 @@
+import pyarrow.parquet as pq
+from args_util import process_args
+import pandas as pd
+import os
+import json
+
+
+class DataPreprocessor(object):
+    def process(self, data_frame: pd.DataFrame):
+        context = data_frame['paragraphs']
+        question = data_frame['question']
+        title = data_frame['title']
+        data = {'data': {'title': title, 'paragraphs': [{'context': context, 'qas': [{'question': question}]}]}}
+        return data
+
+
+if __name__ == '__main__':
+    args = process_args()
+
+    processor = DataPreprocessor()
+    input_file_path = os.path.join(args.input_data, 'data.dataset.parquet')
+    data_frame = pd.read_parquet(input_file_path, engine='pyarrow')
+    data = processor.process(data_frame)
+
+    json.dump(data, open(os.path.join(args.output_dir, 'dev.json'), "w"))
