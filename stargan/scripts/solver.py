63fd54a9bc44b6aa8de252c39bc64f000f44c321
Si Chen <t-chsi@microsoft.com>
2019-08-26 15:06:53 +0800

    update

--- stargan/scripts/solver.py
+++ stargan/scripts/solver.py
@@ -287,11 +287,8 @@ class Solver(object):
         elif self.dataset == 'RaFD':
             data_loader = self.rafd_loader
 
-        print('data_loader', data_loader)
         with torch.no_grad():
             for i, (x_real, c_org) in enumerate(data_loader):
-                print(i, x_real, c_org)
-
                 # Prepare input images and target domain labels.
                 x_real = x_real.to(self.device)
                 c_trg_list = self.create_labels(c_org, self.c_dim, self.dataset, self.selected_attrs)
@@ -305,5 +302,4 @@ class Solver(object):
                 x_concat = torch.cat(x_fake_list, dim=3)
                 result_path = os.path.join(self.result_dir, '{}-images.jpg'.format(i + 1))
                 save_image(self.denorm(x_concat.data.cpu()), result_path, nrow=1, padding=0)
-                print('Saved real and fake images into {}...'.format(result_path))
             print('Saved all real and fake images into {}...'.format(result_path))
