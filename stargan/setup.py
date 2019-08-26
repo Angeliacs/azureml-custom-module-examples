773e69bdd68f8e86211bb11220679d054021f7c2
Si Chen <t-chsi@microsoft.com>
2019-08-26 11:08:55 +0800

    add setup.py

---
+++ stargan/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Stargan Train",
+    version="0.0.1",
+    description="stargan train model",
+    packages=["scripts"],
+    author="Si Chen",
+    license="MIT",
+    include_package_data=True,
+)
